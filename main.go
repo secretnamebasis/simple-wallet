8fbb3f2c8a655e66020ea6c4e8aa9f51e0507721
secretnamebasis <secretnamebasis@keemail.me>
2025-10-25 15:18:03 -0600

    fix: remove unnecessary calls

--- main.go
+++ main.go
@@ -52,12 +52,8 @@ func run() {
 	// this is a single window program, pretty simple
 	program.window = program.application.NewWindow(program.name)
 
-	// and just for fun, let's have a simple viewer window
-	program.explorer = program.application.NewWindow(program.name + " | viewer")
-
 	// let's size the window, I think this is a nice size
 	program.window.Resize(program.size)
-	program.explorer.Resize(program.size)
 
 	// let's center it to make things simple
 	program.window.CenterOnScreen()
