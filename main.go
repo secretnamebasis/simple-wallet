1d12470fc65d82c8026b220ad2e0c9ab913ba8a3
secretnamebasis <secretnamebasis@keemail.me>
2025-12-02 16:21:50 -0700

    fix: move functions from main

--- main.go
+++ main.go
@@ -2,22 +2,15 @@ package main
 
 import (
 	"crypto/rand"
-	"encoding/gob"
 	"fmt"
 	m_rand "math/rand"
 	"os"
-	"path/filepath"
 	"strconv"
-	"sync"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
-	"github.com/chzyer/readline"
-	"github.com/deroproject/derohe/globals"
-	"github.com/deroproject/derohe/walletapi"
-	"github.com/go-logr/logr"
 )
 
 // main caller
@@ -76,6 +69,7 @@ func run() {
 		if program.wallet != nil {
 			program.wallet = nil
 		}
+		logger.Info("closed")
 		os.Exit(0)
 	})
 
@@ -179,95 +173,4 @@ func initialize() {
 
 	// test localhost first, then connect from a list of public nodes
 	go maintain_connection()
-}
-func initialize_logger() {
-	// We need to initialize readline first, so it changes stderr to ansi processor on windows
-	l, err := readline.NewEx(&readline.Config{
-		Prompt: "\033[92mDERO:\033[32m»\033[0m",
-		// Prompt:          prompt,
-		HistoryFile: "", // wallet never saves any history file anywhere, to prevent any leakage
-		// AutoComplete:    completer,
-		InterruptPrompt: "^C",
-		EOFPrompt:       "exit",
-
-		HistorySearchFold: true,
-		// FuncFilterInputRune: filterInput,
-	})
-	if err != nil {
-		panic(err)
-	}
-	defer l.Close()
-
-	// parse arguments and setup logging , print basic information
-	f, err := os.Create(filepath.Join(globals.GetDataDirectory(), program.name+".log"))
-	if err != nil {
-		fmt.Printf("Error while opening log file err: %s filename %s\n", err, program.name+".log")
-		return
-	}
-	globals.InitializeLog(l.Stdout(), f)
-	logger = globals.Logger.WithName(program.name)
-
-}
-
-var logger logr.Logger
-
-var only_once sync.Once
-
-// let's make sure this is only loaded once
-func initialize_table() {
-	only_once.Do(func() {
-		big_table := os.Getenv("USE_BIG_TABLE")
-		handleTable := func(s string, i int) {
-			logger.Info("Please wait, generating precompute table.... ")
-			if loadTable(s) {
-				logger.Info("Loaded lookup table from disk.")
-			} else {
-				msg := "(1<<21)"
-				if big_table != "" {
-					msg = "(1<<24)"
-				}
-				logger.Info("Generating lookup table " + msg + "... this may take a while.")
-				walletapi.Initialize_LookupTable(1, i)
-				saveTable(s, walletapi.Balance_lookup_table)
-			}
-
-			tables := len(*walletapi.Balance_lookup_table)
-			if walletapi.Balance_lookup_table != nil && tables > 0 {
-				tableLen := float64(len((*walletapi.Balance_lookup_table)[0]))
-				logger.Info(fmt.Sprintf("Lookup table info: %d table, with %.f entries (~%.f MiB)",
-					tables, tableLen, tableLen*8/kilobyte/kilobyte))
-			}
-		}
-		if big_table != "" {
-			handleTable("big_table.gob", 1<<24)
-		} else {
-			handleTable("small_table.gob", 1<<21)
-		}
-		logger.Info("Precompute table loaded into memory")
-	})
-}
-
-func loadTable(filename string) bool {
-	f, err := os.Open(filepath.Join(globals.GetDataDirectory(), filename))
-	if err != nil {
-		return false // file not found
-	}
-	defer f.Close()
-	if err := gob.NewDecoder(f).Decode(&walletapi.Balance_lookup_table); err != nil {
-		fmt.Println("Failed to decode lookup table:", err)
-		return false
-	}
-	return true
-}
-
-func saveTable(filename string, table any) {
-	f, err := os.Create(filepath.Join(globals.GetDataDirectory(), filename))
-	if err != nil {
-		fmt.Println("Failed to create table file:", err)
-		return
-	}
-	defer f.Close()
-	if err := gob.NewEncoder(f).Encode(table); err != nil {
-		fmt.Println("Failed to encode lookup table:", err)
-	}
 }
